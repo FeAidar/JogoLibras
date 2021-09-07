454bbb57ec75b1c7338a50378c598f9e91fb0113
JoaoMiguelCardoso <jocardoso2010@gmail.com>
2021-09-07 14:58:27 -0300

    posicionamento das cartas

--- JogoLibras/Assets/Scripts/ManejoDasCartas.cs
+++ JogoLibras/Assets/Scripts/ManejoDasCartas.cs
@@ -4,21 +4,33 @@ using UnityEngine;
 
 public class ManejoDasCartas : MonoBehaviour
 {
-    [SerializeField]private GameObject[] _Cartas;
+    private List<GameObject> _Cartas = new List<GameObject>();
     [SerializeField]private Sprite[] _Sprites;
-    private int _Carta,type, dupla;
-    private Vector3 _away = new Vector3(1000f, 1000f, 0f);
-    private bool a;
+    [SerializeField]private float[] _regulacaos;
+    [SerializeField]private GameObject _CartaEmBranco;
+    private List<Vector3> _posicao = new List<Vector3>();
+    private List<bool> _valido = new List<bool>();
+    [SerializeField]private float addcolum, addlinha;
+    [SerializeField] private Vector3[] comeca;
+    private int _Carta,type, dupla, coluna, linha, possiveis;
+    private float length;
+    private bool a, b = true;
     private GameObject Definer;
     private int _Dificudade;
     private void Start(){
         Definer = GameObject.FindGameObjectWithTag("GameController");
-        _Dificudade = Definer.GetComponent<GameDefiner>().Dificuldade;
+        if(GameObject.FindGameObjectWithTag("GameController") != null){
+            if(Definer.GetComponent<GameDefiner>() != null){
+                _Dificudade = Definer.GetComponent<GameDefiner>().Dificuldade;
+            }
+        }
+        Regula();
         SetCarts();
+        length = _Cartas.Count;
+        Scrumble();
     }
     private void Update(){
         int types = _Sprites.Length;
-        int length = _Cartas.Length;
         if(a){
             if(type <= types){
                 if(type<=types){
@@ -31,10 +43,6 @@ public class ManejoDasCartas : MonoBehaviour
                         type +=1;
                         dupla =0;
                     } 
-                }else{
-
-                    _Cartas[_Carta].transform.Translate(_away, Space.World);
-                    _Carta +=1;
                 }
             }else{
                 a= false;
@@ -45,9 +53,134 @@ public class ManejoDasCartas : MonoBehaviour
         }
     }
     public void Scrumble(){
-
+        /*
+            2. um random para ver qual posção aquela carta vai
+            3. verifica c tem alguma carta naquela posição, se tiver faz o 1 dnv
+            4. se não tiver, aquela posição vai mudar a bool para true 
+            5. aquela carta vai para a posição livre
+        */
+        possiveis = _posicao.Count;
+        for (int i = 0; i < _posicao.Count; i++)
+        {
+            verifica(i);
+        }
+    }
+    private void verifica(int i){
+        int esse = Random.Range(0, possiveis);
+        if(_valido[esse] == true){
+            _Cartas[i].transform.Translate(_posicao[esse], Space.World);
+            _valido[esse] = false;
+        }else{
+            verifica(i);
+        }
     }
 
+    private void Regula(){
+        switch(_Dificudade){
+            case 0:
+                for (int i = 0; i < _regulacaos[0]; i++)
+                {
+                    if(_CartaEmBranco != null){
+                        _Cartas.Add(Instantiate(_CartaEmBranco, transform.position, Quaternion.identity));
+                        if(coluna == 0 && linha == 0 && b == true){
+                            _posicao.Add(new Vector3(comeca[0].x, comeca[0].y, 0f));
+                            _valido.Add(true);
+                            linha++;
+                            b = true;
+                        }else if(coluna == 0 && linha > 0 && b == true){
+                            _posicao.Add(new Vector3(comeca[0].x, _posicao[i-1].y + addlinha, 0f));
+                            _valido.Add(true);
+                            linha++;
+                            b = true;
+                        }else if(b == true){
+                            _posicao.Add(new Vector3(_posicao[i-1].x, _posicao[i-1].y + addlinha, 0f));
+                            _valido.Add(true);
+                            linha++;
+                            b = true;
+                        }else if(b == false){
+                            _posicao.Add(new Vector3(_posicao[i-1].x + addcolum, _posicao[i-4].y, 0f));
+                            _valido.Add(true);
+                            linha++;
+                            b = true;
+                        }
+                        if(linha >= 4){
+                            coluna ++;
+                            linha = 0;
+                            b = false;
+                        }
+                    }
+                }
+            break;
+            case 1:
+                for (int i = 0; i < _regulacaos[1]; i++)
+                {
+                    if(_CartaEmBranco != null){
+                        _Cartas.Add(Instantiate(_CartaEmBranco, transform.position, Quaternion.identity));
+                        if(coluna == 0 && linha == 0 && b == true){
+                            _posicao.Add(new Vector3(comeca[1].x, comeca[1].y, 0f));
+                            _valido.Add(true);
+                            linha++;
+                            b = true;
+                        }else if(coluna == 0 && linha > 0 && b == true){
+                            _posicao.Add(new Vector3(comeca[1].x, _posicao[i-1].y + addlinha, 0f));
+                            _valido.Add(true);
+                            linha++;
+                            b = true;
+                        }else if(b == true){
+                            _posicao.Add(new Vector3(_posicao[i-1].x, _posicao[i-1].y + addlinha, 0f));
+                            _valido.Add(true);
+                            linha++;
+                            b = true;
+                        }else if(b == false){
+                            _posicao.Add(new Vector3(_posicao[i-1].x + addcolum, _posicao[i-4].y, 0f));
+                            _valido.Add(true);
+                            linha++;
+                            b = true;
+                        }
+                        if(linha >= 4){
+                            coluna ++;
+                            linha = 0;
+                            b = false;
+                        }
+                    }
+                }
+            break;
+            case 2:
+                for (int i = 0; i < _regulacaos[2]; i++)
+                {
+                    if(_CartaEmBranco != null){
+                        _Cartas.Add(Instantiate(_CartaEmBranco, transform.position, Quaternion.identity));
+                        if(coluna == 0 && linha == 0){
+                            _posicao.Add(new Vector3(comeca[2].x, comeca[2].y, 0f));
+                            _valido.Add(true);
+                            linha++;
+                            b = true;
+                        }else if(coluna == 0 && linha > 0){
+                            _posicao.Add(new Vector3(comeca[2].x, _posicao[i-1].y + addlinha, 0f));
+                            _valido.Add(true);
+                            linha++;
+                            b = true;
+                        }else if(b == true){
+                            _posicao.Add(new Vector3(_posicao[i-1].x, _posicao[i-1].y + addlinha, 0f));
+                            _valido.Add(true);
+                            linha++;
+                            b = true;
+                        }else if(b == false){
+                            _posicao.Add(new Vector3(_posicao[i-1].x + addcolum, _posicao[i-4].y, 0f));
+                            _valido.Add(true);
+                            linha++;
+                            b = true;
+                        }
+                        if(linha >= 4){
+                            coluna ++;
+                            linha = 0;
+                            b = false;
+                        }
+                    }
+                }
+            break;
+        }
+    }
     public void SetCarts(){
         a = true;
     }
