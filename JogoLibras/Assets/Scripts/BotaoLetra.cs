0341649900406fb46e165b8bd54fb2b5eff62fad
JoaoMiguelCardoso <jocardoso2010@gmail.com>
2021-10-01 09:56:31 -0300

    etapas no alfabeto

--- JogoLibras/Assets/Scripts/BotaoLetra.cs
+++ JogoLibras/Assets/Scripts/BotaoLetra.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BotaoLetra : MonoBehaviour
 {
     [SerializeField]private int numero;
+    [SerializeField][Tooltip("0 = com a letra escrita, 1 = sem a letra escrita")]private Sprite[] etapas;
     public char letra;
     public void aciona(){
         GameObject.FindGameObjectWithTag("canvas").GetComponent<AlfabetoManejo>().BTM_add(numero);
+    }
+    private void Start(){
+        if(GameObject.FindGameObjectWithTag("GameController").GetComponent<GameDefiner>().Etapa == 0){
+            GetComponent<Image>().sprite = etapas[0];
+        }else{
+            GetComponent<Image>().sprite = etapas[1];
+        }
     }
 
 }
--- JogoLibras/Assets/Scripts/palavra.cs
+++ JogoLibras/Assets/Scripts/palavra.cs
@@ -5,4 +5,15 @@ using UnityEngine;
 public class palavra : MonoBehaviour
 {
     public string Palavra;
+    [SerializeField][Tooltip("")]private GameObject[] etapas;
+    
+    private void Start(){
+        if(GameObject.FindGameObjectWithTag("GameController").GetComponent<GameDefiner>().Etapa >= 2){
+            etapas[0].SetActive(false);
+            etapas[1].SetActive(true);
+        }else{
+            etapas[1].SetActive(false);
+            etapas[0].SetActive(true);
+        }
+    }
 }
