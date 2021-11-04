41538860a3987018f14bc6519cc6fbb4f3216935
FeAidar <phelaidar@gmail.com>
2021-11-04 16:00:18 -0300

    Introdução
    Começo da Introdução do jogo

---
+++ JogoLibras/Assets/TransicaoEntreCena.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransicaoEntreCena : MonoBehaviour
+{
+    private transicao Transicao;
+    private GameObject t;
+    void Start()
+    {
+
+    }
+
+    private void OnEnable()
+
+    {
+        t = GameObject.FindGameObjectWithTag("Transicao");
+        Transicao = t.GetComponent<transicao>();
+        Debug.Log(t);
+
+        if (t.GetComponent<CanvasGroup>().alpha == (0))
+            Transicao.inicia();
+        else
+                Transicao.finaliza();
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
